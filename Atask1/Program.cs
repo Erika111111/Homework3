148aba6da3e42f80ad9d884421d05aeb8fb6a33a
Erika <Erika1111@yandex.ru>
2022-12-16 14:36:52 +0300

    Homework 3

---
+++ Atask1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if(len == 5)
+{
+    if( number[0] == number[4] && number[1] == number[3])
+        {
+        Console.WriteLine(" Число является палиндромом");
+        }
+    else 
+    {
+    Console.WriteLine("Число не является палиндромом");
+    }
+}
---
+++ Atask2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты X1 ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y1 ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z1 ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X2 ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y2 ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z2 ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double DistXY(int x1, int y1, int z1, int x2,int y2, int z2)
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)+Math.Pow((z1-z2),2));
+    return result;
+}
+
+Console.WriteLine($"Расстояние между точками = {DistXY(x1,y1,z1,x2,y2,z2)}");
